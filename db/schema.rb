9b9df523c737139f694b189d45e43fec84182380
Yuming He <yuming.p.he@gmail.com>
2016-10-30 17:14:55 -0400

    added bio and shipping cap, add bestbudcanada to seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161029210223) do
+ActiveRecord::Schema.define(version: 20161030193844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,8 @@ ActiveRecord::Schema.define(version: 20161029210223) do
     t.string   "help_desk_type",    default: "NONE"
     t.string   "help_desk_api_url"
     t.string   "help_desk_auth"
+    t.string   "bio"
+    t.decimal  "free_shipping_cap"
   end
 
   create_table "users", force: :cascade do |t|
