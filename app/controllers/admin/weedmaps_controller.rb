59e47bdda10b4060ea2b6706f9fcba91e5db0b46
Han Ke <michael.kehan@gmail.com>
2016-11-03 00:05:39 -0400

    cleaned up weedmaps parse

--- app/controllers/admin/weedmaps_controller.rb
+++ app/controllers/admin/weedmaps_controller.rb
@@ -2,12 +2,11 @@ class Admin::WeedmapsController < ApplicationController
   def parse_menu
     @retailer = Retailer.find(params[:retailer_id])
     @response = weedmaps_menu params[:menu_path]
-    Retailer.persist_menu @response
+    @retailer.persist_menu @response
   end
 
   def weedmaps_menu(menu_path)
     conn = Faraday.new(:url => 'https://weedmaps.com') do |faraday|
-      faraday.request  :url_encoded             # form-encode POST params
       faraday.response :logger                  # log requests to STDOUT
       faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
     end
@@ -15,9 +14,8 @@ class Admin::WeedmapsController < ApplicationController
     response = conn.get do |req|
       req.url menu_path
     end
-
-    puts response
-    response.body
+    
+    JSON.parse(response.body)
   end
 
   def parse_menu_params
