611c529f772bc3d2a90dc274aa7d4617b878cb22
Yuming He <yuming.p.he@gmail.com>
2016-11-19 17:55:22 -0500

    ..

--- app/controllers/admin/weedmaps_controller.rb
+++ app/controllers/admin/weedmaps_controller.rb
@@ -20,21 +20,20 @@ class Admin::WeedmapsController < ApplicationController
 
   def add_dispensary()
     # TODO: Use this to add retailers
-    # @retailer = Retailer.create!({
-    #     image_url: 'http://oregoncannabisinsurance.com/wp-content/uploads/2014/04/oregon-cannabis-dispensary-business-insurance.jpg',
-    #     name: 'My Cannabis Canada',
-    #     address: 'Hamilton, ON',
-    #     bio: 'Recreational for 19+',
-    #     description: 'We are a 19+ ONLY recreational mobile dispensary we ship canada wide. We offer the best quality medicine at compassionate prices!',
-    #     mail: true,
-    #     delivery: false,
-    #     pickup: false,
-    #     shipping_fee: 21,
-    #     free_shipping_cap: 350,
-    #     email: "peter@nimbusfly.co",
-    #     phone_number: "647-853-8697",
-    #     medical: false
-    #   })
+    @retailer = Retailer.create!({
+        image_url: 'https://s3.amazonaws.com/media.nimbusfly.co/default/default.jpg',
+        name: 'Dispensary',
+        address: 'Toronto, ON',
+        bio: 'Comming soon',
+        description: 'We are a 19+ ONLY recreational mobile dispensary we ship canada wide. We offer the best quality medicine at compassionate prices!',
+        mail: true,
+        delivery: false,
+        pickup: false,
+        shipping_fee: 0,
+        email: "peter@nimbusfly.co",
+        phone_number: "666-666-6666",
+        medical: false
+      })
   end
 
   def parse_menu_params
